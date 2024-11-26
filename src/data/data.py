1e065dec521e31ff70a2b2a38882d8cc6f49342f
Joshua Jang <musicofmusix@gmail.com>
2024-11-26 13:34:30 -0500

    Add additional data processing in data.py
    In the preprocessed CSV, some rows have missing features. Now, data.py will drop all such rows before creating the training and testing sets.
    Also, date, time, and categorical (string) based features are now numerically encoded.

--- src/data/data.py
+++ src/data/data.py
@@ -3,6 +3,7 @@ import csv
 import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
 
 FILE_PATH = 'toronto_crime_data.csv'
 SEED = 311
@@ -18,13 +19,49 @@ def _load_csv(path):
     return df
 
 
+def process_features(df):
+    df['REPORT_DATE'] = pd.to_datetime(df['REPORT_DATE'], errors='coerce')
+    df['Year'] = df['REPORT_DATE'].dt.year
+    df['Month'] = df['REPORT_DATE'].dt.month
+    df['Day'] = df['REPORT_DATE'].dt.day
+    df['REPORT_DOW_NUM'] = df['REPORT_DOW'].map({
+        'Monday': 1, 'Tuesday': 2, 'Wednesday': 3, 'Thursday': 4,
+        'Friday': 5, 'Saturday': 6, 'Sunday': 7
+    })
+
+    # drop all rows with missing features
+    df = df.dropna()
+
+    # convert string-based/categorical features in to numbers
+    # NOT one-hot encoding, but 0...n-1 encoding.
+    # suitable Decision Trees/Random Forest/KDE.
+    categorical_cols = ['PREMISES_TYPE', 'NBH_DESIGNATION', 'OFFENCE']
+    label_encoders = {}
+    for col in categorical_cols:
+        le = LabelEncoder()
+        df[col] = le.fit_transform(df[col])
+        label_encoders[col] = le
+
+    features = [
+        'REPORT_DOY', 'REPORT_HOUR', 'PREMISES_TYPE', 'HOOD_158',
+        'AVG_AGE', 'POPULATION', 'INCOME', 'EMPLOYMENT_RATE',
+        'Year', 'Month', 'Day', 'REPORT_DOW_NUM', 'IsWeekend'
+    ]
+    target = 'OFFENCE'
+
+    X = df[features].to_numpy()
+    y = df[target].to_numpy()
+
+    return X, y, label_encoders
+
+
 def load_data(path):
+    # shuffle-n-split
     df = _load_csv(path)
-    
-    shuffled_df = df.sample(frac=1, random_state=SEED).reset_index(drop=True)
-    labels = shuffled_df.loc[:,"OFFENCE"].to_numpy()
-    data = shuffled_df.drop(['OFFENCE'], axis=1).to_numpy()
-    
+    data, labels, label_encoders = process_features(df)
+    shuffled_indices = np.random.permutation(len(data))
+    data, labels = data[shuffled_indices], labels[shuffled_indices]
+
     return split_data(data, labels)
 
 
