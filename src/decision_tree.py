9ba776a05d4d74533423dbc4961b18eff0797163
Joshua Jang <musicofmusix@gmail.com>
2024-11-27 11:25:15 -0500

    Add decision tree & random forest with some basic parameters

---
+++ src/decision_tree.py
@@ -0,0 +1,30 @@
+from data.data import load_data
+
+import numpy as np
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score
+
+FILE_PATH = 'data/toronto_crime_data.csv'
+SEED = 311
+np.random.seed(SEED)
+
+X_train, y_train, X_val, y_val, X_test, y_test = load_data(FILE_PATH)
+
+# TODO: use a loop to go through different hyperparameters w/ validation set
+decision_tree = DecisionTreeClassifier(
+    max_depth=10,
+    min_samples_split=10,
+    random_state=SEED
+)
+
+decision_tree.fit(X_train, y_train)
+
+# evaluate on validation set
+val_predictions = decision_tree.predict(X_val)
+val_accuracy = accuracy_score(y_val, val_predictions)
+print(f"Validation Accuracy: {val_accuracy:.2f}")
+
+# evaluate on test set
+test_predictions = decision_tree.predict(X_test)
+test_accuracy = accuracy_score(y_test, test_predictions)
+print(f"Test Accuracy: {test_accuracy:.2f}")
---
+++ src/random_forest.py
@@ -0,0 +1,31 @@
+from data.data import load_data
+
+import numpy as np
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+
+FILE_PATH = 'data/toronto_crime_data.csv'
+SEED = 311
+np.random.seed(SEED)
+
+X_train, y_train, X_val, y_val, X_test, y_test = load_data(FILE_PATH)
+
+# TODO: use a loop to go through different hyperparameters w/ validation set
+random_forest = RandomForestClassifier(
+    n_estimators=20,
+    max_depth=8,
+    min_samples_split=5,
+    random_state=SEED,
+)
+
+random_forest.fit(X_train, y_train)
+
+# evaluate on validation set
+val_predictions = random_forest.predict(X_val)
+val_accuracy = accuracy_score(y_val, val_predictions)
+print(f"Validation Accuracy: {val_accuracy:.2f}")
+
+# evaluate on test set
+test_predictions = random_forest.predict(X_test)
+test_accuracy = accuracy_score(y_test, test_predictions)
+print(f"Test Accuracy: {test_accuracy:.2f}")
