955c7ed07a403b4e06d06812a447085fe2e09392
Junhee Lee <jheelee020421@gmail.com>
2024-11-28 16:38:00 -0500

    bnn

---
+++ src/bnn/bnn_clean.py
@@ -0,0 +1,176 @@
+from utils import *
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+from tqdm import tqdm
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from sklearn.compose import ColumnTransformer
+from sklearn.metrics import accuracy_score
+
+import torch
+import torch.nn as nn
+import torchbnn as bnn
+import torch.optim as optim
+from torch.utils.data import DataLoader, TensorDataset
+
+
+class BNN(nn.Module):
+    def __init__(self, input_dim, output_dim, 
+                 lr=0.01, prior_mu=0.0, prior_sigma=0.1):
+        super(BNN, self).__init__()
+        self.bnn_linear1 = bnn.BayesLinear(in_features=input_dim, out_features=32, prior_mu=prior_mu,
+                                           prior_sigma=prior_sigma)
+        self.bnn_linear2 = bnn.BayesLinear(in_features=32, out_features=16, prior_mu=prior_mu, prior_sigma=prior_sigma)
+        self.bnn_linear3 = bnn.BayesLinear(in_features=16, out_features=output_dim, prior_mu=prior_mu,
+                                           prior_sigma=prior_sigma)
+        self.relu = nn.ReLU()
+        self.sigmoid = nn.Sigmoid()
+
+        self.model = nn.Sequential(
+            self.bnn_linear1,
+            self.relu,
+            self.bnn_linear2,
+            self.relu,
+            self.bnn_linear3,
+            self.sigmoid
+        )
+        
+        self.optimizer = torch.optim.Adam(self.parameters(), lr=lr)
+        self.criterion = nn.BCELoss()
+        self.kl_loss = bnn.BKLLoss(reduction="mean", last_layer_only=False)
+
+    def forward(self, x):
+        return self.model(x)
+
+
+class BatchNormBNN(nn.Module):
+    def __init__(self, input_dim, output_dim, 
+                 lr=0.01, prior_mu=0.0, prior_sigma=0.1):
+        super(BatchNormBNN, self).__init__()
+        self.bnn_linear1 = bnn.BayesLinear(in_features=input_dim, out_features=32, prior_mu=prior_mu,
+                                           prior_sigma=prior_sigma)
+        self.bnn_norm1 = nn.BatchNorm1d(32)
+        self.bnn_linear2 = bnn.BayesLinear(in_features=32, out_features=16, prior_mu=prior_mu, prior_sigma=prior_sigma)
+        self.bnn_norm2 = nn.BatchNorm1d(16)
+        self.bnn_linear3 = bnn.BayesLinear(in_features=16, out_features=output_dim, prior_mu=prior_mu,
+                                           prior_sigma=prior_sigma)
+        self.relu = nn.ReLU()
+        self.sigmoid = nn.Sigmoid()
+
+        self.model = nn.Sequential(
+            self.bnn_linear1,
+            self.bnn_norm1,
+            self.bnn_linear2,
+            self.bnn_norm2,
+            self.bnn_linear3,
+            self.sigmoid
+        )
+        
+        self.optimizer = torch.optim.Adam(self.parameters(), lr=lr)
+        self.criterion = nn.BCELoss()
+        self.kl_loss = bnn.BKLLoss(reduction="mean", last_layer_only=False)
+
+    def forward(self, x):
+        return self.model(x)
+
+
+def batch_train(model, X_train, y_train, X_val, y_val, num_epochs, batch_size):
+    train_dl = DataLoader(TensorDataset(X_train, y_train), batch_size=batch_size)
+
+    losses = []
+    val_losses = []
+    val_accuracies = []
+
+    # for epoch in tqdm(range(num_epochs)):
+    for epoch in range(num_epochs):
+        print(f"----- Epoch {epoch + 1} -----")
+        avg_loss = 0
+        for x, y in train_dl:
+            # forward pass
+            outputs = model(x)
+            bce_loss = model.criterion(outputs, y)
+            kl_loss = model.kl_loss(model)
+            total_loss = bce_loss + 0.1 * kl_loss
+            avg_loss += total_loss.item()
+
+            # backward pass + optimisation
+            model.optimizer.zero_grad()
+            total_loss.backward()
+            model.optimizer.step()
+        
+            model.train()
+        avg_loss /= len(train_dl)
+        losses.append(avg_loss)
+
+        val_loss, val_acc = validation(model, X_val, y_val)
+        val_losses.append(val_loss)
+        val_accuracies.append(val_acc)
+
+    plot_loss(range(num_epochs), losses, val_losses)
+    plot_acc(range(num_epochs), val_accuracies)
+
+
+def train(model, X_train, y_train, X_val, y_val, num_epochs):
+    losses = []
+    val_losses = []
+    val_accuracies = []
+
+    # for epoch in tqdm(range(num_epochs)):
+    for epoch in range(num_epochs):
+        print(f"----- Epoch {epoch + 1} -----")
+        # forward pass
+        outputs = model(X_train)
+        bce_loss = model.criterion(outputs, y_train)
+        kl_loss = model.kl_loss(model)
+        total_loss = bce_loss + 0.1 * kl_loss
+        losses.append(total_loss.item())
+
+        # backward pass + optimisation
+        model.optimizer.zero_grad()
+        total_loss.backward()
+        model.optimizer.step()
+    
+        model.train()
+    
+        val_loss, val_acc = validation(model, X_val, y_val)
+        val_losses.append(val_loss)
+        val_accuracies.append(val_acc)
+
+    plot_loss(range(num_epochs), losses, val_losses)
+    plot_acc(range(num_epochs), val_accuracies)
+
+
+def validation(model, x, y):
+    model.eval()
+    with torch.no_grad():
+        outputs = model(x)
+        pred = (outputs >= 0.5).float()
+        loss = model.criterion(pred, y)
+        accuracy = accuracy_score(y, pred)
+        print(f"Validation Loss: {loss.item():.4f}")
+        print(f"Validation Accuracy: {accuracy:.2%}")
+    
+    return loss.item(), accuracy
+
+
+if __name__ == '__main__':
+    X_train, y_train, X_val, y_val, X_test, y_test = load_tensor()
+
+    lr = 0.01
+    batch_size = 256
+    num_epochs = 10
+    prior_sigma = 0.1
+    prior_mu = 0.
+
+    input_dim = X_train.shape[1]
+    output_dim = 1
+
+    model = BNN(input_dim=input_dim, output_dim=output_dim, 
+                prior_sigma=prior_sigma, prior_mu=prior_mu, lr=lr)
+    # model = BatchNormBNN(input_dim=input_dim, output_dim=output_dim)
+
+    train(model, X_train, y_train, X_val, y_val, num_epochs)
+    # batch_train(model, X_train, y_train, X_val, y_val, num_epochs, batch_size)
+
+    
---
+++ src/bnn/utils.py
@@ -0,0 +1,93 @@
+from data import *
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import StandardScaler, OneHotEncoder, normalize
+import matplotlib.pyplot as plt
+
+
+def load_tensor():
+    # WE DO NOT USE load_data (WHICH INCLUDES 0...N-1 ENCODING)
+    X_train, y_train, X_val, y_val, X_test, y_test = load_df_data()
+
+    # preprocessing for numerical and categorical features
+    num_features = ["REPORT_YEAR", "REPORT_MONTH", "REPORT_DAY", 
+                    "REPORT_DOW", "REPORT_DOY", "REPORT_HOUR", 
+                    "LONGITUDE", "LATITUDE", "AVG_AGE", "POPULATION", 
+                    "INCOME", "EMPLOYMENT_RATE"]
+    cat_features = ["PREMISES_TYPE"]
+
+    preprocessor = ColumnTransformer(
+        transformers=[
+            ("num", StandardScaler(), num_features),  # standardize numerical features
+            ("cat", OneHotEncoder(), cat_features),  # one-hot encode categorical features
+        ]
+    )
+
+    # preprocessing
+    X_train = preprocessor.fit_transform(X_train)
+    X_val = preprocessor.transform(X_val)
+    X_test = preprocessor.transform(X_test)
+
+    for x in [X_train, X_val, X_test]:
+        normalize(x, norm='l1')
+
+    # plt.hist(X_train)
+    # plt.savefig("src/bnn_train.png")
+
+    # plot_features(X_train)
+
+    categorical_encoder = preprocessor.named_transformers_['cat']  # Extract the OneHotEncoder
+
+    X_train_tensor = torch.tensor(X_train, dtype=torch.float32)
+    X_val_tensor = torch.tensor(X_val, dtype=torch.float32)
+    X_test_tensor = torch.tensor(X_test, dtype=torch.float32)
+
+    y_train_tensor = torch.tensor(y_train.values, dtype=torch.float32).unsqueeze(1)
+    y_val_tensor = torch.tensor(y_val.values, dtype=torch.float32).unsqueeze(1)
+    y_test_tensor = torch.tensor(y_test.values, dtype=torch.float32).unsqueeze(1)
+
+    return X_train_tensor, y_train_tensor, X_val_tensor, y_val_tensor, X_test_tensor, y_test_tensor
+
+
+def plot_features(x):
+    nrows, ncols = 3, 4
+    figure, axis = plt.subplots(nrows, ncols, figsize=(20, 15))
+    for i in range(nrows):
+        for j in range(ncols):
+            k = i * ncols + j
+
+            if k >= x.shape[1]:
+                break
+
+            feature = FEATURES[k]
+            d = x[:, k]
+            axis[i, j].hist(d, bins='auto')
+            axis[i, j].set_title(feature)
+
+    plt.tight_layout()
+    plt.savefig("src/bnn_data.png")
+
+
+def plot_loss(iteration, losses, val_losses):
+    plt.figure(1)
+    plt.subplot(211)
+    plt.plot(iteration, losses, label="Training Loss", color="orange")
+    plt.ylabel("Training Loss")
+
+    plt.subplot(212)
+    plt.plot(iteration, val_losses, label="Validation Loss")
+    plt.xlabel("Iteration")
+    plt.ylabel("Validation Loss")
+
+    plt.savefig("src/bnn_loss.png")
+
+
+def plot_acc(iteration, accuracy):
+    plt.figure(2)
+    plt.plot(iteration, accuracy, label="Validation Accuracy")
+    plt.xlabel("Iteration")
+    plt.ylabel("Validation Accuracy")
+    plt.savefig("src/bnn_accuracy.png")
+
+
+if __name__ == '__main__':
+    load_tensor()
