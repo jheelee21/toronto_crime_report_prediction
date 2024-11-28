9f636ad438446a3c68cf3eae3ef5ebe333ac09d9
Joshua Jang <musicofmusix@gmail.com>
2024-11-27 21:33:00 -0500

    Add hyperparameter grid search to DT and RF

--- src/decision_tree_random_forest/decision_tree.py
+++ src/decision_tree_random_forest/decision_tree.py
@@ -4,27 +4,60 @@ import numpy as np
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import accuracy_score
 
-FILE_PATH = '../data/toronto_crime_data.csv'
 SEED = 311
 np.random.seed(SEED)
 
-X_train, y_train, X_val, y_val, X_test, y_test = load_data(FILE_PATH)
+X_train, y_train, X_val, y_val, X_test, y_test = load_data()
 
-# TODO: use a loop to go through different hyperparameters w/ validation set
-decision_tree = DecisionTreeClassifier(
-    max_depth=10,
-    min_samples_split=10,
-    random_state=SEED
-)
+# for grid search
+Max_depth = [2, 10, 100]
+Min_samples_split = [2, 10, 100]
 
-decision_tree.fit(X_train, y_train)
+# best performer init
+best_model = None
+best_params = {}
+best_accuracy = 0
 
-# evaluate on validation set
-val_predictions = decision_tree.predict(X_val)
-val_accuracy = accuracy_score(y_val, val_predictions)
-print(f"Validation Accuracy: {val_accuracy:.2f}")
+# 2nd-best performer init
+runnerup_model = None
+runnerup_params = {}
 
-# evaluate on test set
-test_predictions = decision_tree.predict(X_test)
-test_accuracy = accuracy_score(y_test, test_predictions)
-print(f"Test Accuracy: {test_accuracy:.2f}")
+# do the grid search, find best performer
+for max_depth in Max_depth:
+    for min_samples_split in Min_samples_split:
+        print(f"Testing: max_depth={max_depth}, min_samples_split={min_samples_split}")
+
+        decision_tree = DecisionTreeClassifier(
+            max_depth=max_depth,
+            min_samples_split=min_samples_split,
+            random_state=SEED
+        )
+        decision_tree.fit(X_train, y_train)
+
+        # evaluate on the validation set
+        val_predictions = decision_tree.predict(X_val)
+        val_accuracy = accuracy_score(y_val, val_predictions)
+
+        # update if a better model is found
+        if val_accuracy > best_accuracy:
+            runnerup_model = best_model
+            runnerup_params = best_params
+            best_model = decision_tree
+
+            best_params = {
+                'max_depth': max_depth,
+                'min_samples_split': min_samples_split,
+            }
+            best_accuracy = val_accuracy
+
+# evaluate best model on test set
+best_test_predictions = best_model.predict(X_test)
+best_test_accuracy = accuracy_score(y_test, best_test_predictions)
+print(f"Best model test Accuracy: {best_test_accuracy:.2f} with max_depth={best_params['max_depth']}, "
+      f"min_samples_split={best_params['min_samples_split']}")
+
+# evaluate runner-up model on test set
+runnerup_test_predictions = runnerup_model.predict(X_test)
+runnerup_test_predictions = accuracy_score(y_test, runnerup_test_predictions)
+print(f"Runner-up model test Accuracy: {runnerup_test_predictions:.2f} with max_depth={runnerup_params['max_depth']}, "
+      f"min_samples_split={runnerup_params['min_samples_split']}")
--- src/decision_tree_random_forest/random_forest.py
+++ src/decision_tree_random_forest/random_forest.py
@@ -4,28 +4,65 @@ import numpy as np
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
 
-FILE_PATH = '../data/toronto_crime_data.csv'
 SEED = 311
 np.random.seed(SEED)
 
-X_train, y_train, X_val, y_val, X_test, y_test = load_data(FILE_PATH)
+X_train, y_train, X_val, y_val, X_test, y_test = load_data()
 
-# TODO: use a loop to go through different hyperparameters w/ validation set
-random_forest = RandomForestClassifier(
-    n_estimators=20,
-    max_depth=8,
-    min_samples_split=5,
-    random_state=SEED,
-)
+# for grid search
+N_estimators = [10, 20, 40]
+Max_depth = [2, 10, 100]
+Min_samples_split = [2, 10, 100]
 
-random_forest.fit(X_train, y_train)
+# best performer init
+best_model = None
+best_params = {}
+best_accuracy = 0
 
-# evaluate on validation set
-val_predictions = random_forest.predict(X_val)
-val_accuracy = accuracy_score(y_val, val_predictions)
-print(f"Validation Accuracy: {val_accuracy:.2f}")
+# 2nd-best performer init
+runnerup_model = None
+runnerup_params = {}
+
+# do the grid search, find best performer
+for n_estimators in N_estimators:
+    for max_depth in Max_depth:
+        for min_samples_split in Min_samples_split:
+            print(f"Testing: n_estimators={n_estimators}, max_depth={max_depth}, "
+                  f"min_samples_split={min_samples_split}")
+
+            random_forest = RandomForestClassifier(
+                n_estimators=n_estimators,
+                max_depth=max_depth,
+                min_samples_split=min_samples_split,
+                random_state=SEED
+            )
+            random_forest.fit(X_train, y_train)
+
+            # evaluate on the validation set
+            val_predictions = random_forest.predict(X_val)
+            val_accuracy = accuracy_score(y_val, val_predictions)
+
+            # update if a better model is found
+            if val_accuracy > best_accuracy:
+                runnerup_model = best_model
+                runnerup_params = best_params
+                best_model = random_forest
+                best_params = {
+                    'n_estimators': n_estimators,
+                    'max_depth': max_depth,
+                    'min_samples_split': min_samples_split,
+                }
+                best_accuracy = val_accuracy
 
 # evaluate on test set
-test_predictions = random_forest.predict(X_test)
-test_accuracy = accuracy_score(y_test, test_predictions)
-print(f"Test Accuracy: {test_accuracy:.2f}")
+best_test_predictions = best_model.predict(X_test)
+best_test_accuracy = accuracy_score(y_test, best_test_predictions)
+print(f"Best model test Accuracy: {best_test_accuracy:.2f} with n_estimators={best_params['n_estimators']}, "
+      f"max_depth={best_params['max_depth']}, min_samples_split={best_params['min_samples_split']}")
+
+# evaluate runner-up model on test set
+runnerup_test_predictions = runnerup_model.predict(X_test)
+runnerup_test_predictions = accuracy_score(y_test, runnerup_test_predictions)
+print(f"Runner-up model test Accuracy: {runnerup_test_predictions:.2f} with n_estimators={runnerup_params['n_estimators']}, "
+      f"max_depth={runnerup_params['max_depth']}, min_samples_split={runnerup_params['min_samples_split']}")
+
