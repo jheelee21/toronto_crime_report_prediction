048b7dd02d26d1efc45e90a04bcc1908c34ac809
Joshua Jang <musicofmusix@gmail.com>
2024-11-29 17:27:15 -0500

    Update decision tree to include criterion parameter tuning

--- src/decision_tree_random_forest/decision_tree.py
+++ src/decision_tree_random_forest/decision_tree.py
@@ -12,6 +12,7 @@ X_train, y_train, X_val, y_val, X_test, y_test = load_data()
 # for grid search
 Max_depth = [2, 10, 100]
 Min_samples_split = [2, 10, 100]
+Criterions = ["gini", "entropy", "log_loss"]
 
 # best performer init
 best_model = None
@@ -25,39 +26,43 @@ runnerup_params = {}
 # do the grid search, find best performer
 for max_depth in Max_depth:
     for min_samples_split in Min_samples_split:
-        print(f"Testing: max_depth={max_depth}, min_samples_split={min_samples_split}")
+        for criterion in Criterions:
+            print(f"Testing: max_depth={max_depth}, min_samples_split={min_samples_split}, criterion={criterion}")
 
-        decision_tree = DecisionTreeClassifier(
-            max_depth=max_depth,
-            min_samples_split=min_samples_split,
-            random_state=SEED
-        )
-        decision_tree.fit(X_train, y_train)
+            decision_tree = DecisionTreeClassifier(
+                max_depth=max_depth,
+                min_samples_split=min_samples_split,
+                criterion=criterion,
+                random_state=SEED
+            )
+            decision_tree.fit(X_train, y_train)
 
-        # evaluate on the validation set
-        val_predictions = decision_tree.predict(X_val)
-        val_accuracy = accuracy_score(y_val, val_predictions)
+            # evaluate on the validation set
+            val_predictions = decision_tree.predict(X_val)
+            val_accuracy = accuracy_score(y_val, val_predictions)
 
-        # update if a better model is found
-        if val_accuracy > best_accuracy:
-            runnerup_model = best_model
-            runnerup_params = best_params
-            best_model = decision_tree
+            # update if a better model is found
+            if val_accuracy > best_accuracy:
+                runnerup_model = best_model
+                runnerup_params = best_params
+                best_model = decision_tree
 
-            best_params = {
-                'max_depth': max_depth,
-                'min_samples_split': min_samples_split,
-            }
-            best_accuracy = val_accuracy
+                best_params = {
+                    'max_depth': max_depth,
+                    'min_samples_split': min_samples_split,
+                    'criterion': criterion
+                }
+                best_accuracy = val_accuracy
 
 # evaluate best model on test set
 best_test_predictions = best_model.predict(X_test)
 best_test_accuracy = accuracy_score(y_test, best_test_predictions)
 print(f"Best model test Accuracy: {best_test_accuracy:.2f} with max_depth={best_params['max_depth']}, "
-      f"min_samples_split={best_params['min_samples_split']}")
+      f"min_samples_split={best_params['min_samples_split']}, criterion={best_params['criterion']}")
 
 # evaluate runner-up model on test set
-runnerup_test_predictions = runnerup_model.predict(X_test)
-runnerup_test_predictions = accuracy_score(y_test, runnerup_test_predictions)
-print(f"Runner-up model test Accuracy: {runnerup_test_predictions:.2f} with max_depth={runnerup_params['max_depth']}, "
-      f"min_samples_split={runnerup_params['min_samples_split']}")
+if runnerup_model is not None:
+    runnerup_test_predictions = runnerup_model.predict(X_test)
+    runnerup_test_predictions = accuracy_score(y_test, runnerup_test_predictions)
+    print(f"Runner-up model test Accuracy: {runnerup_test_predictions:.2f} with max_depth={runnerup_params['max_depth']}, "
+          f"min_samples_split={runnerup_params['min_samples_split']}, criterion={runnerup_params['criterion']}")
